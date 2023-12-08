524fb12f53a85b65dbb52e834a5d80856feea8b4
Dmytro Chernomord <dchernomord@i-doit.com>
2023-12-08 15:48:55 +0100

    add tasks

---
+++ src/generic/1.ts
@@ -0,0 +1,17 @@
+/*
+  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
+  Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
+*/
+
+function getPromise (): Promise<[string , number]> {
+  return new Promise((resolve):void => {
+    resolve(['Text', 50]);
+  });
+}
+
+getPromise()
+.then((data):void => {
+  console.log(data);
+});
+
+export {};
---
+++ src/generic/4.ts
@@ -0,0 +1,19 @@
+/*
+  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+*/
+interface IProps {
+  title: string;
+}
+class Component<T> {
+  constructor (public props:T) {
+
+  }
+}
+
+class Page extends Component<IProps> {
+  pageInfo ():void {
+    console.log(this.props.title);
+  }
+}
+
+export {};
